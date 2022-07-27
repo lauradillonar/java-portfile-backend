94c19fa17ccc473d951a7a75d0c9c1376734a728
lauradillonar <laura.dillon.ar@gmail.com>
2022-07-27 16:35:08 -0300

    first commit

---
+++ src/main/java/com/confluenciacreativa/portfile/PortfileApplication.java
@@ -0,0 +1,13 @@
+package com.confluenciacreativa.portfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/confluenciacreativa/portfile/PortfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.confluenciacreativa.portfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
